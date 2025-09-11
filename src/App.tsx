5d6a852e78f3dcc0af4ac8179c43021b2f0c24bb
Lucila Anchoverri <anchoverrilucila15@gmail.com>
2025-09-11 16:29:54 -0300

    fin del trabajo miercoles 10/09 (login-logica)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import Login from './components/pages/registro/login/Login'
-
+import Registro from './components/pages/registro/Registro'
 function App() {
 
   return (
     <>
-    <Login/>
-
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />  {/* Página de inicio */}
+          <Route path="/registro" element={<Registro/>} /> {/* Página del perfil */}
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
