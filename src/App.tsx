ab4bac854f0f42fb59fc3bf49d1ad6a774fa7fd4
Lucila Anchoverri <anchoverrilucila15@gmail.com>
2025-09-16 16:32:30 -0300

    acomodo orden de carpetas y archivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
-import Login from './components/pages/registro/login/Login'
+import Login from './components/pages/login/Login'
 import Registro from './components/pages/registro/Registro'
 function App() {
 
