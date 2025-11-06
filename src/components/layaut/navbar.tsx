844c7fd8c585f6ba38d9dce2be525c198af1bb92
BelenAntunez2024 <beluantunez99@gmail.com>
2025-11-06 19:40:46 -0300

    Actualizacion

--- src/components/layaut/navbar.tsx
+++ src/components/layaut/navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <div className="nav-left">
           <HamburgerMenu/>
         </div>
-
+        
         {/* Logo en el centro */}
         <div className="nav-center">
           <h1 className="logo">WISTERIA</h1>
