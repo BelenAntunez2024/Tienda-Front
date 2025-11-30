5d51e046256bf42a08a91d4efc5038a24a1bff90
BelenAntunez2024 <beluantunez99@gmail.com>
2025-11-30 20:44:12 -0300

    Cmbios

--- src/components/layout/HamburgerMenu.tsx
+++ src/components/layout/HamburgerMenu.tsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 
 const HamburgerMenu: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
+  
 
   const toggleMenu = () => setIsOpen(!isOpen);
 
