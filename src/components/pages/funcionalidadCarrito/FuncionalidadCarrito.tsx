ac4e47aa54f56f2f02f97322c69f683b199e0953
Lucila Anchoverri <anchoverrilucila15@gmail.com>
2025-11-30 19:41:26 -0300

    estilo final de carrito.

--- src/components/pages/funcionalidadCarrito/FuncionalidadCarrito.tsx
+++ src/components/pages/funcionalidadCarrito/FuncionalidadCarrito.tsx
@@ -1,45 +1,47 @@
 import { useState, useEffect } from "react";
 import type { CarritoItem } from "./interfaces/CarritoItem";
 import '../verProductos/ProductCard.css';
+import VolverAtras from "../../layout/VolverAtras";
+import './FuncionalidadCarrito.css'
 
 const FuncionalidadCarrito = () => {
 
     const [carrito, setCarrito] = useState<CarritoItem[]>([]);
-    
+
     //carga los items del carrito desde el backend
     useEffect(() => {
-    const fetchCarrito = async () => {
-        try {
-            const token = localStorage.getItem('token') || '';
-            const response = await fetch('http://localhost:3000/item-ordenes', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
+        const fetchCarrito = async () => {
+            try {
+                const token = localStorage.getItem('token') || '';
+                const response = await fetch('http://localhost:3000/item-ordenes', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+                if (!response.ok) {
+                    throw new Error(`Error: ${response.status}`);
                 }
-            });
-            if (!response.ok) {
-                throw new Error(`Error: ${response.status}`);
+                const data = await response.json();
+                const mappedCarrito = data.map((item: any) => ({
+                    ...item.producto,  // Copia todos los campos del producto
+                    cantidad: item.cantidad_productos,  // Renombra el campo
+                    id_item_orden: item.id_item_orden
+                }));
+                console.log('Datos mapeados:', mappedCarrito);
+                setCarrito(mappedCarrito);
+            } catch (err) {
+                console.error('Error al obtener el carrito:', err);
             }
-        const data = await response.json();
-        const mappedCarrito = data.map((item: any) => ({
-            ...item.producto,  // Copia todos los campos del producto
-            cantidad: item.cantidad_productos,  // Renombra el campo
-            id_item_orden: item.id_item_orden
-        }));
-        console.log('Datos mapeados:', mappedCarrito);
-        setCarrito(mappedCarrito);
-        } catch (err) {
-            console.error('Error al obtener el carrito:', err);
-        }
-    };
-    fetchCarrito();
-}, []);
+        };
+        fetchCarrito();
+    }, []);
 
     const actualizarCantidad = async (idItemOrden: number, nuevaCantidad: number) => {
         const token = localStorage.getItem('token') || '';
         const response = await fetch(`http://localhost:3000/item-ordenes/${idItemOrden}`, {
-            method: 'PATCH', 
+            method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${token}`
@@ -56,7 +58,7 @@ const FuncionalidadCarrito = () => {
 
     const eliminar = async (idItemOrden: number) => {
         const token = localStorage.getItem('token') || '';
-        
+
         const response = await fetch(`http://localhost:3000/item-ordenes/orden/${idItemOrden}`, {
             method: 'DELETE',
             headers: {
@@ -66,23 +68,23 @@ const FuncionalidadCarrito = () => {
 
         if (!response.ok) {
             throw new Error(`Error ${response.status}: No se pudo eliminar el producto.`);
-        }        
+        }
     };
     //llama al DELETE y elimina el producto del estado del carrito
     const eliminarDelCarrito = async (idItemOrden: number, productoId: number) => {
         try {
-            await eliminar(idItemOrden); 
+            await eliminar(idItemOrden);
             setCarrito(prevCarrito => prevCarrito.filter((item) => item.id_producto !== productoId));
-            
+
             console.log(`Producto eliminado del carrito, ID: ${productoId}`);
-            
+
         } catch (err) {
             console.error("Error al eliminar:", err);
         }
     }
 
     const restarCantidad = async (productoId: number, idItemOrden: number) => {
-        const producto = carrito.find((item) => item.id_producto === productoId); 
+        const producto = carrito.find((item) => item.id_producto === productoId);
         if (!producto) {
             return "El producto no está en el carrito";
         }
@@ -90,16 +92,16 @@ const FuncionalidadCarrito = () => {
             if (producto.cantidad > 1) {
                 const nuevaCantidad = producto.cantidad - 1;
                 await actualizarCantidad(idItemOrden, nuevaCantidad);
-                
-                setCarrito(prevCarrito => prevCarrito.map(item => 
-                    item.id_producto === productoId 
-                        ? { ...item, cantidad: nuevaCantidad } 
+
+                setCarrito(prevCarrito => prevCarrito.map(item =>
+                    item.id_producto === productoId
+                        ? { ...item, cantidad: nuevaCantidad }
                         : item
                 ));
                 alert("Cantidad actualizada con éxito.");
-            } else if (producto.cantidad === 1) { 
+            } else if (producto.cantidad === 1) {
                 // Si la cantidad es 1, llamamos a la función de eliminación (DELETE)
-                await eliminarDelCarrito(idItemOrden, productoId); 
+                await eliminarDelCarrito(idItemOrden, productoId);
             }
         } catch (error) {
             console.error("Error al restar cantidad:", error);
@@ -107,7 +109,7 @@ const FuncionalidadCarrito = () => {
     }
 
     const sumarCantidad = async (productoId: number, idItemOrden: number) => {
-        const producto = carrito.find((item) => item.id_producto === productoId); 
+        const producto = carrito.find((item) => item.id_producto === productoId);
         if (!producto) {
             return "El producto no está en el carrito";
         }
@@ -115,10 +117,10 @@ const FuncionalidadCarrito = () => {
             const nuevaCantidad = producto.cantidad + 1;
             await actualizarCantidad(idItemOrden, nuevaCantidad);
 
-            setCarrito(prevCarrito => prevCarrito.map(item => 
-                item.id_producto === productoId 
+            setCarrito(prevCarrito => prevCarrito.map(item =>
+                item.id_producto === productoId
                     ? { ...item, cantidad: nuevaCantidad } // Actualiza la cantidad
-                    : item 
+                    : item
             ));
             alert("Cantidad actualizada con éxito.");
         } catch (error) {
@@ -128,39 +130,43 @@ const FuncionalidadCarrito = () => {
 
     return (
         <main>
-            <h1> Carrito </h1>
+            <VolverAtras hasNavbar={true} />
+            <h1 className="carrito-titulo"> Carrito </h1>
 
-    <div>
-        <h2>Productos elegidos</h2>
-        <div>
-        {carrito.map((producto) => (
-            <div key={producto.id_item_orden} className="div-product-card">
-                <h2>{producto.nombre}</h2>
-                <img src={producto.imagen} alt={producto.nombre} width={200} />
-                <p className="p-description">{producto.descripcion}</p>
-                <p className="p-precio">
-                    Precio: ${(producto.precio * producto.cantidad).toFixed(2)}
-                </p>                                            
-                <p>Cantidad: {producto.cantidad}</p>
+            <div className='carrito-container'>
+                <h2 className="carrito-subtitulo">Productos elegidos</h2>
+                <div>
+                    {carrito.length === 0 ? (
+                        <p className='carrito-vacio'>Carrito vacío</p>
+                    ) : (
+                        carrito.map((producto) => (
+                            <div key={producto.id_item_orden} className="div-product-card">
+                                <h2>{producto.nombre}</h2>
+                                <img src={producto.imagen} alt={producto.nombre} width={200} />
+                                <p className="p-description">{producto.descripcion}</p>
+                                <p className="p-precio">
+                                    Precio: ${(producto.precio * producto.cantidad).toFixed(2)}
+                                </p>
+                                <p>Cantidad: {producto.cantidad}</p>
+                                <button
+                                    onClick={() => sumarCantidad(producto.id_producto, producto.id_item_orden)}>
+                                    +
+                                </button>
+
+                                <button
+                                    onClick={() => restarCantidad(producto.id_producto, producto.id_item_orden)}>
+                                    -
+                                </button>
+
+                                <button
+                                    onClick={() => eliminarDelCarrito(producto.id_item_orden, producto.id_producto)}>
+                                    Eliminar
+                                </button>
+                            </div>
+                        )))}
                     
-                    <button 
-                        onClick={() => sumarCantidad(producto.id_producto, producto.id_item_orden)}>
-                        +
-                    </button>
-                    
-                    <button 
-                        onClick={() => restarCantidad(producto.id_producto, producto.id_item_orden)}>
-                        -
-                    </button>
-                    
-                    <button 
-                        onClick={() => eliminarDelCarrito(producto.id_item_orden, producto.id_producto)}>
-                        Eliminar
-                    </button>
                 </div>
-            ))}
-        </div>
-    </div>
+            </div>
         </main>
     )
 }
